f9afa309de8262a401ebfb92f0b2aabffb24e3d4
DurasovN <nikolay.durasov@gmail.com>
2025-11-16 02:11:01 +0400

    Add: main.py and wrote some RAG pipline

---
+++ main.py
@@ -0,0 +1,27 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+# Посмотреть скачанные модели: ollama list
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are an expert in answering questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is the question to asnwer: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+
+while True:
+    print("\n\n#############################################")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n#############################################")
+    if question == "q":
+        break
+    result = chain.invoke({"reviews": [], "question": question})
+
+    print(result)
